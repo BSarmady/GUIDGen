15a1b254b00198f0cad363dfffea16f51f4430c8
Bob <bsarmady@yahoo.com>
2022-10-28 19:05:54 +0800

    Optimizing code - Changed project type to winexe to avoid showing console window when using the app. - Moved help text to help method.

--- GUIDGen/Program.cs
+++ GUIDGen/Program.cs
@@ -4,6 +4,7 @@ using System.Windows.Forms;
 
 internal static class Program {
 
+    #region internal static int Main(...)
     [STAThread]
     internal static int Main(string[] args) {
 
@@ -12,34 +13,15 @@ internal static class Program {
             try {
                 mode = Convert.ToInt32(args[0].ToLower().Replace("mode=", ""));
             } catch {
-                Console.WriteLine("Creates a new GUID and copies it to clipboard");
-                Console.WriteLine("Usage:");
-                Console.WriteLine("    GuidGen [mode=1~6]");
-                Console.WriteLine();
-                Console.WriteLine("    mode is optional number between 1 to 6 to create guid in different formats as following");
-                Console.WriteLine("      1: String");
-                Console.WriteLine("             Example: {CFB78D94-052F-46F5-87C3-0133A7B280BA}");
-                Console.WriteLine();
-                Console.WriteLine("      2: Array of 16 hex bytes");
-                Console.WriteLine("             Example: new byte[] { 0x94, 0x8D, 0xB7, 0xCF, 0x2F, 0x05, 0xF5, 0x46, 0x87, 0xC3, 0x01, 0x33, 0xA7, 0xB2, 0x80, 0xBA }");
-                Console.WriteLine();
-                Console.WriteLine("      3: 16 bytes array is base 10 numbers");
-                Console.WriteLine("             Example: new byte[] { 148, 141, 183, 207, 47, 5, 245, 70, 135, 195, 1, 51, 167, 178, 128, 186 }");
-                Console.WriteLine();
-                Console.WriteLine("      4: int, short , short, byte[8]");
-                Console.WriteLine("             Example: -810054252, 1327, 18165, new byte[] { 135, 195, 1, 51, 167, 178, 128, 186 }");
-                Console.WriteLine();
-                Console.WriteLine("      5: int, short, short, byte, byte, byte, byte, byte, byte, byte, byte");
-                Console.WriteLine("             Example: -810054252, 1327, 18165, 135, 195, 1, 51, 167, 178, 128, 186");
-                Console.WriteLine();
-                Console.WriteLine("      6: uint, ushort, ushort, byte, byte, byte, byte, byte, byte, byte, byte");
-                Console.WriteLine("             Example: 0xCFB78D94, 0x052F, 0x46F5, 0x87, 0xC3, 0x01, 0x33, 0xA7, 0xB2, 0x80, 0xBA");
+                Help();
             }
         }
         Clipboard.SetText(GenerateGuidText(Guid.NewGuid(), mode));
         return 0;
     }
+    #endregion
 
+    #region internal static string GenerateGuidText(...)
     internal static string GenerateGuidText(Guid guid, int mode) {
         StringBuilder sb = new StringBuilder();
         byte[] bytearr = guid.ToByteArray();
@@ -117,4 +99,33 @@ internal static class Program {
 
         return sb.ToString().Replace(", }", "}").TrimEnd(' ', ',');
     }
+    #endregion
+
+    #region private static void Help()
+    private static void Help() {
+        Console.WriteLine("Creates a new GUID and copies it to clipboard");
+        Console.WriteLine("Usage:");
+        Console.WriteLine("    GuidGen [mode=1~6]");
+        Console.WriteLine();
+        Console.WriteLine("    mode is optional number between 1 to 6 to create guid in different formats as following");
+        Console.WriteLine("      1: String");
+        Console.WriteLine("             Example: CFB78D94-052F-46F5-87C3-0133A7B280BA");
+        Console.WriteLine();
+        Console.WriteLine("      2: Array of 16 hex bytes");
+        Console.WriteLine("             Example: new byte[] { 0x94, 0x8D, 0xB7, 0xCF, 0x2F, 0x05, 0xF5, 0x46, 0x87, 0xC3, 0x01, 0x33, 0xA7, 0xB2, 0x80, 0xBA }");
+        Console.WriteLine();
+        Console.WriteLine("      3: 16 bytes array is base 10 numbers");
+        Console.WriteLine("             Example: new byte[] { 148, 141, 183, 207, 47, 5, 245, 70, 135, 195, 1, 51, 167, 178, 128, 186 }");
+        Console.WriteLine();
+        Console.WriteLine("      4: int, short , short, byte[8]");
+        Console.WriteLine("             Example: -810054252, 1327, 18165, new byte[] { 135, 195, 1, 51, 167, 178, 128, 186 }");
+        Console.WriteLine();
+        Console.WriteLine("      5: int, short, short, byte, byte, byte, byte, byte, byte, byte, byte");
+        Console.WriteLine("             Example: -810054252, 1327, 18165, 135, 195, 1, 51, 167, 178, 128, 186");
+        Console.WriteLine();
+        Console.WriteLine("      6: uint, ushort, ushort, byte, byte, byte, byte, byte, byte, byte, byte");
+        Console.WriteLine("             Example: 0xCFB78D94, 0x052F, 0x46F5, 0x87, 0xC3, 0x01, 0x33, 0xA7, 0xB2, 0x80, 0xBA");
+    }
+    #endregion
+
 }
